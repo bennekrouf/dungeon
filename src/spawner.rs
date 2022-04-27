00c1ad0bdc98ac63726f51a2ce273ba4b034c750
Bennekrouf <mohamed.bennekrouf@gmail.com>
2022-04-27 17:01:06 +0200

    Move monsters randomly

--- src/spawner.rs
+++ src/spawner.rs
@@ -25,7 +25,8 @@ pub fn spawn_monster(ecs: &mut World, pos: Point, rng: &mut RandomNumberGenerato
                     2 => to_cp437('o'),
                     _ => to_cp437('g'),
                 }
-            }
+            },
+            MovingRandomly{},
         )
     );
 }
